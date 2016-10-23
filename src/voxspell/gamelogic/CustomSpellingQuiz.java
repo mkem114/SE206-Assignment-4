ec9ffd10f82b1d49df9ce864f7fab36985cdec0f
mkem114 <mkem114@aucklanduni.ac.nz>
2016-10-23 20:28:36 +1300

    Word Randomisation
    Custom and New Spelling Quizzes now quiz in a random order
    Words that need to be worked on are pushed to the front so that the user works on them

--- src/voxspell/gamelogic/CustomSpellingQuiz.java
+++ src/voxspell/gamelogic/CustomSpellingQuiz.java
@@ -1,5 +1,8 @@
 package voxspell.gamelogic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -13,6 +16,28 @@ public class CustomSpellingQuiz extends SpellingQuiz {
 
     @Override
     protected List<QuizWord> getWords() {
-        return level().words();
+        List<QuizWord> rawList = level().words();
+        Collections.shuffle(rawList);
+        if (rawList.size() <= 20) {
+            return rawList;
+        } else {
+            List<QuizWord> workingList = new ArrayList<>(rawList);
+            workingList.sort(new Comparator<QuizWord>() {
+                public int compare(QuizWord word1, QuizWord word2) {
+                    if (word1.timesAttempted() > word2.timesAttempted()) {
+                        return 1;
+                    } else if (word1.timesAttempted() == word2.timesAttempted()) {
+                        return 0;
+                    }
+                    return -1;
+                }
+            });
+            List<QuizWord> playList = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                playList.add(workingList.get(i));
+            }
+            Collections.shuffle(playList);
+            return playList;
+        }
     }
 }
--- src/voxspell/gamelogic/NewSpellingQuiz.java
+++ src/voxspell/gamelogic/NewSpellingQuiz.java
@@ -1,5 +1,8 @@
 package voxspell.gamelogic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -31,7 +34,36 @@ public class NewSpellingQuiz extends SpellingQuiz {
 	 */
 	@Override
 	protected List<QuizWord> getWords() {
-		return level().words();
+		List<QuizWord> rawList = level().words();
+		Collections.shuffle(rawList);
+		if (rawList.size() <= 10) {
+			return rawList;
+		} else {
+			List<QuizWord> workingList = new ArrayList<>(rawList);
+			workingList.sort(new Comparator<QuizWord>() {
+				public int compare(QuizWord word1, QuizWord word2) {
+					if (word1.timesFailed() > word2.timesFailed()) {
+						return 1;
+					} else if (word1.timesFailed() == word2.timesFailed()) {
+						if (word1.timesFaulted() > word2.timesFaulted()) {
+							return 1;
+						} else if (word1.timesFaulted() == word2.timesFaulted()) {
+							if (word2.timesAttempted() > word1.timesAttempted()) {
+								return 1;
+							} else if (word2.timesAttempted() == word1.timesAttempted()) {
+								return 0;
+							}
+						}
+					}
+					return -1;
+				}
+			});
+			List<QuizWord> playList = new ArrayList<>();
+			for (int i = 0; i < 10; i++) {
+				playList.add(workingList.get(i));
+			}
+			Collections.shuffle(playList);
+			return playList;
+		}
 	}
-
 }
